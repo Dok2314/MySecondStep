876dda318ab9c327043cf4b05e6f2a32dab5f888
Dok2314 <koversundanil@gmail.com>
2022-11-26 21:30:44 +0200

    Test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,7 @@ class HomeController extends Controller
 {
     public function home(PostFilter $filter)
     {
+        dd('test');
         $posts    = Post::filter($filter)->paginate(5);
         $users    = User::all();
 
