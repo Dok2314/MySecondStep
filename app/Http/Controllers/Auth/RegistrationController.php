d366cb32fcd406d5268de1cc72a88ac22e39858a
Dok2314 <koversundanil@gmail.com>
2022-12-08 18:40:39 +0200

    Added ratelimiting for login

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -6,12 +6,8 @@ use App\Events\UserRegistered;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegistrationRequest;
 use App\Models\User;
-use App\Services\Helpers\Telegram;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Events\Validated;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use function dd;
 use function view;
 
 class RegistrationController extends Controller
