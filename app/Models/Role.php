9087d58f3a86021f02c20b5cb0de254ccd37fd1d
Dok2314 <koversundanil@gmail.com>
2022-12-15 20:24:37 +0200

    Added accessor and mutator to role and post

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Role extends Model
 {
@@ -13,5 +14,18 @@ class Role extends Model
     public function permissions()
     {
         return $this->belongsToMany(Permission::class, 'role_permission','role_id','permission_id');
+    }
+
+    protected function name(): Attribute
+    {
+        $enValues = [
+            'Администратор' => 'admin',
+            'Разработчик' => 'developer',
+            'Пользователь' => 'user',
+        ];
+
+        return Attribute::make(
+            get: fn($value) => ucfirst($enValues[$value])
+        );
     }
 }
